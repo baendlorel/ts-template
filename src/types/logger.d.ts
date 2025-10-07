2ba97d26bf4691db874d063de6bea2e695f6eaf3
aldia_mechdream <futami16237@gmail.com>
2025-10-07 13:04:40 +0800

    feat: logger增加函数

--- src/types/logger.d.ts
+++ src/types/logger.d.ts
@@ -3,6 +3,8 @@ declare namespace logger {
   function warn(...message: any[]): void;
   function error(...message: any[]): void;
   function debug(...message: any[]): void;
+  function succ(...message: any[]): void;
+  function verbose(...message: any[]): void;
   function WorkspaceNotFound(id: string): void;
   function TabNotFoundInWorkspace(id: string, tabId: number): void;
 }
